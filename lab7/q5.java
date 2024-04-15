8e3d6fb287804ef3b2ade65a127e0e1914ada571
Mayankmishra63 <158036143+Mayankmishra63@users.noreply.github.com>
2024-04-15 20:34:00 +0530

    Create q5.java

---
+++ lab7/q5.java
@@ -0,0 +1,32 @@
+package day7;
+
+/**
+ *
+ * @author acer
+ */
+class Base{
+    void display(){
+        System.out.println("In base display");
+    }
+    
+    void show(){
+        
+    }
+}
+class Derived extends Base{
+    void display(){
+        System.out.println("In derived display");
+    }
+    void show(){
+        System.out.println("In derived Show");
+    }
+}
+public class q5 {
+    public static void main(String[] args) {
+        Base obj1=new Base();
+        Base obj2=new Derived();
+        obj1.display();
+        obj2.display();
+        obj2.show();
+    }
+}
